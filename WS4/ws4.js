ae40f15ac60f526e0093741432c6b40ccd14c6b0
Nandy Bâ <nandyba@yahoo.fr>
2021-02-01 16:37:01 +0100

    WS4.1 Add zoom animation
    Zoom animation added. It's also compatible with others animations

--- WS4/ws4.js
+++ WS4/ws4.js
@@ -11,7 +11,11 @@ $bgEndColor			=	document.getElementById("bgEndColor");
 
 $startButton		=	document.getElementById("start");
 
+$zoomButton			=	document.getElementById("zoom");
+
 // Animation
+// get div to must be animate
+$tobeanimated		=	document.getElementById("tobeanimated");
 
 $startButton.onclick = function(){
 	// Get animation colors
@@ -24,8 +28,7 @@ $startButton.onclick = function(){
 
 	bg_end_color		=	$bgEndColor.value;
 
-	// get div to must be animate
-	$tobeanimated		=	document.getElementById("tobeanimated");
+	
 
 	// Animate the div
 	// Information: we cannot write it directly in the css file because animation depends to user input and must be change without form submition
@@ -45,7 +48,11 @@ $startButton.onclick = function(){
 	}';
 	style.innerHTML = keyFrames.replace(/A_DYNAMIC_VALUE/g, "180deg");
 	document.getElementsByTagName('head')[0].appendChild(style);
-	$tobeanimated.style.setProperty("animation", 'animateColor 5s linear');
+	$tobeanimated.style.setProperty("animation", getComputedStyle($tobeanimated).animation+', animateColor 5s linear');
 	setInterval(function(){ style.remove(); }, 5000); //renome added style to be able to reuse the function
 	
+}
+
+$zoomButton.onclick = function(){
+	$tobeanimated.style.setProperty("animation", getComputedStyle($tobeanimated).animation+', scaleAnimation 5s linear');
 }
