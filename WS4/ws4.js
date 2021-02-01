e560a91136d2915693b3307aee744554bc4c7d7c
Nandy Bâ <nandyba@yahoo.fr>
2021-02-01 11:51:35 +0100

    WS4.1 preparation

---
+++ WS4/ws4.js
@@ -0,0 +1,20 @@
+
+// Select elements
+
+$borderColor		=	document.getElementById("borderColor");
+
+$bgColor			=	document.getElementById("bgColor");
+
+$boderEndColor		=	document.getElementById("boderEndColor");	
+
+$bgEndColor			=	document.getElementById("bgEndColor");
+
+// Get animation colors
+
+border_start_color	=	$borderColor.value;
+
+bg_start_color		=	$bgColor.value;
+
+border_end_color	=	$boderEndColor.value;
+
+bg_end_color		=	$bgEndColor.value;
