e67b6ccd9116bc755f44b50b5c6f6c7924daed41
Nandy Bâ <nandyba@yahoo.fr>
2021-02-01 13:16:36 +0100

    WS4 add of animateColor animation
    An animation base on user input

--- WS4/ws4.js
+++ WS4/ws4.js
@@ -24,4 +24,28 @@ $startButton.onclick = function(){
 
 	bg_end_color		=	$bgEndColor.value;
 
+	// get div to must be animate
+	$tobeanimated		=	document.getElementById("tobeanimated");
+
+	// Animate the div
+	// Information: we cannot write it directly in the css file because animation depends to user input and must be change without form submition
+
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	var keyFrames = '\
+	@-webkit-keyframes animateColor {\
+		0%, 100% {\
+			background-color: '+bg_start_color+';\
+			border-color: '+border_start_color+'\
+		}\
+		50% {\
+		background-color: '+bg_end_color+';\
+		border-color: '+border_end_color+'\
+		}\
+	}';
+	style.innerHTML = keyFrames.replace(/A_DYNAMIC_VALUE/g, "180deg");
+	document.getElementsByTagName('head')[0].appendChild(style);
+	$tobeanimated.style.setProperty("animation", 'animateColor 5s linear');
+	setInterval(function(){ style.remove(); }, 5000); //renome added style to be able to reuse the function
+	
 }
